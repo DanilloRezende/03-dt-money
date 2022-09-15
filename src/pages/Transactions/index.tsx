10ecc35462fd9ca6878ece55a51202b564c241d2
DanilloRezende <danillo.rezende3@gmail.com>
2022-09-15 16:25:25 -0300

    Conexão com a API - Realizando requisição HTTP

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Searchform } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { PriceHighligth, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
+
+    interface Transaction {
+        id: number;
+        description: string;
+        type: "income" | "outcome";
+        price: number;
+        category: string;
+        createdAt: string;
+    }
+
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+
+    async function loadTransaction() {
+        const response = await fetch('http://localhost:3000/transactions')
+        const data = await response.json();
+        
+        setTransactions(data)
+        }
+    
+    useEffect(() => {
+        loadTransaction();
+    }, [])
+
     return (
         <div>
             <Header />
@@ -13,26 +37,20 @@ export function Transactions() {
             <Searchform />
            <TransactionsTable>
                 <tbody>
-                    <tr>
-                        <td width='50%'>Desenvolvimento de Site</td>
+                    {transactions.map(transactions => {
+                        return (
+                            <tr key={transactions.id}>
+                        <td width='50%'>{transactions.description}</td>
                         <td>
-                            <PriceHighligth variant="income">
-                                R$ 12.000,00
+                            <PriceHighligth variant={transactions.type}>
+                                {transactions.price}
                             </PriceHighligth>
                         </td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
+                        <td>{transactions.category}</td>
+                        <td>{transactions.createdAt}</td>
                     </tr>
-                    <tr>
-                        <td width='50%'>Aluguel</td>
-                        <td>
-                            <PriceHighligth variant="outcome">
-                                - R$ 1.200,00
-                            </PriceHighligth>
-                        </td>
-                        <td>Moradia</td>
-                        <td>10/04/2022</td>
-                    </tr>
+                        )
+                    })}
                 </tbody>
             </TransactionsTable>
            </TransactionsContainer>
