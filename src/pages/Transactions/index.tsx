45872460b2dc4fae156c884eeabe983633000b73
DanilloRezende <danillo.rezende3@gmail.com>
2022-09-15 16:45:13 -0300

    Conexão com a API - Criando contexto de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,32 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Searchform } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
+import { TransactionContext } from "../../contexts/TransactionsContext";
 import { PriceHighligth, TransactionsContainer, TransactionsTable } from "./styles";
 
-export function Transactions() {
-
-    interface Transaction {
-        id: number;
-        description: string;
-        type: "income" | "outcome";
-        price: number;
-        category: string;
-        createdAt: string;
-    }
-
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-
-    async function loadTransaction() {
-        const response = await fetch('http://localhost:3000/transactions')
-        const data = await response.json();
-        
-        setTransactions(data)
-        }
-    
-    useEffect(() => {
-        loadTransaction();
-    }, [])
+export function Transactions() {   
+    const { transactions } = useContext(TransactionContext)
 
     return (
         <div>
