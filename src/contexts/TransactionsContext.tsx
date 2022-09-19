0401f0701f0cb2d519ac7f34b02b544bdbe8c4da
DanilloRezende <danillo.rezende3@gmail.com>
2022-09-19 16:31:25 -0300

    Conexão com a API - Aplicando useMemo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -61,7 +61,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
     useEffect(() => {
         fetchTransactions();
-    }, [])
+    }, [fetchTransactions])
     return (
 
         <TransactionContext.Provider value={{
